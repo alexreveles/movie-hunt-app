3810db94c018426f1789df6c0720e13200377626
alex <alexreveles@yahooo.com>
2021-02-03 20:52:38 -0800

    update

--- assets/script.js
+++ assets/script.js
@@ -1,153 +1,134 @@
 $(document).ready(function () {
-    const omdbApiKey = '382c68b3' 
-    const nyTimesApiKey = 'r7DoTIKRHJVeJcAn7wGvTFoc3EDJSEAA'
+  const omdbApiKey = "382c68b3";
+  const nyTimesApiKey = "r7DoTIKRHJVeJcAn7wGvTFoc3EDJSEAA";
 
-    let searchMovieInput = document.querySelector("#search-movie")
-    let searchMovieButton = document.querySelector("#search-movie-btn")
+  let searchMovieInput = document.querySelector("#search-movie");
+  let searchMovieButton = document.querySelector("#search-movie-btn");
 
-    let currentMovie = document.querySelector("#currentMovie")
+  let currentMovie = document.querySelector("#currentMovie");
 
-    let movieSearchHistory = document.querySelector(".movie-history-list")
+  let movieSearchHistory = document.querySelector(".movie-history-list");
 
-    let clearHistory = document.querySelector("#clear-history")
+  let clearHistory = document.querySelector("#clear-history");
 
-    let myMovieSearchHistory
+  let myMovieSearchHistory;
 
-    let moviesArray = [] //localStorage.getItem('movieTitleKey') ///[]
-    let test = localStorage.getItem('movieTitleKey')
-    moviesArray.push(test)
+  let moviesArray = []; //localStorage.getItem('movieTitleKey') ///[]
+  let test = localStorage.getItem("movieTitleKey");
+  moviesArray.push(test);
 
-    ///add eventListener when user inputs a search
-    searchMovieButton.addEventListener("click", function (e) {
-        e.preventDefault()
-        let searchResult = searchMovieInput.value
-        
-        let movieTitle = document.createElement('p')
-        movieTitle.textContent = searchResult
-      
-        moviesArray.push(searchResult) //movieTitle.textContent
-        console.log(moviesArray)
+  ///add eventListener when user inputs a search
+  searchMovieButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    let searchResult = searchMovieInput.value;
 
-        let myMovieSearchHistory = document.createElement('button')
-        myMovieSearchHistory.textContent = searchResult
-        movieSearchHistory.appendChild(myMovieSearchHistory)
+    let movieTitle = document.createElement("p");
+    movieTitle.textContent = searchResult;
 
-        getMovieTitle(movieTitle.textContent)
+    moviesArray.push(searchResult); //movieTitle.textContent
+    console.log(moviesArray);
 
-        saveMovieTitle(movieTitle.textContent)
-        
+    let myMovieSearchHistory = document.createElement("button");
+    myMovieSearchHistory.textContent = searchResult;
+    movieSearchHistory.appendChild(myMovieSearchHistory);
 
-    });
+    getMovieTitle(movieTitle.textContent);
 
+    saveMovieTitle(movieTitle.textContent);
+  });
 
+  //retries the movie title based on the search, using omdb api
+  let getMovieTitle = function (search) {
+    fetch(`https://www.omdbapi.com/?apikey=${omdbApiKey}&t=${search}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
 
+        currentMovie.innerHTML = "";
 
+        let myMovieImg = document.createElement("img");
+        myMovieImg.id = "div-movie-img";
+        myMovieImg.setAttribute("src", `${data.Poster}`);
+        currentMovie.appendChild(myMovieImg);
 
-    //retries the movie title based on the search, using omdb api
-    let getMovieTitle = function (search) {
+        let myMoviePlot = document.createElement("p");
+        myMoviePlot.textContent = `Plot: ${data.Plot}`;
+        myMoviePlot.id = "div-movie-plot";
+        console.log(myMoviePlot.id);
+        currentMovie.appendChild(myMoviePlot);
+
+        let myMovieYear = document.createElement("p");
+        myMovieYear.textContent = `Year: ${data.Year}`;
+        myMovieYear.id = "div-movie-year";
+        currentMovie.appendChild(myMovieYear);
+
+        let myMovieRating = document.createElement("p");
+        myMovieRating.textContent = `Rating: ${data.Rated}`;
+        myMovieRating.id = "div-movie-rating";
+        currentMovie.appendChild(myMovieRating);
+
+        let myMovieGenre = document.createElement("p");
+        myMovieGenre.textContent = `Genre: ${data.Genre}`;
+        myMovieGenre.id = "div-movie-genre";
+        currentMovie.appendChild(myMovieGenre);
+
+        ///created another fetch call to display movie review from NY times API
+
         fetch(
-            `https://www.omdbapi.com/?apikey=${omdbApiKey}&t=${search}`
-        ).then(function (response) {
+          `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${search}&api-key=${nyTimesApiKey}`
+        )
+          .then(function (response) {
             return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
+          })
+          .then(function (data) {
+            console.log(data); //.results[0].link
 
-            currentMovie.innerHTML = '';
+            let movieReview = document.createElement("a");
+            movieReview.textContent = `Movie Review from NY Times:`;
+            movieReview.id = "div-movie-review";
+            movieReview.setAttribute("href", `${data.results[0].link.url}`);
+            currentMovie.appendChild(movieReview);
+          });
+      });
+  };
 
-            let myMovieImg = document.createElement('img')
-            myMovieImg.id = 'div-movie-img'
-            myMovieImg.setAttribute('src', `${data.Poster}`)
-            currentMovie.appendChild(myMovieImg)
+  ////function to save movie titles to local storage, to be updated later
+  let saveMovieTitle = function (search) {
+    localStorage.setItem("movieTitleKey", JSON.stringify(moviesArray));
+  };
 
-            let myMoviePlot = document.createElement('p')
-            myMoviePlot.textContent = `Plot: ${data.Plot}`
-            myMoviePlot.id = 'div-movie-plot'
-            console.log(myMoviePlot.id)
-            currentMovie.appendChild(myMoviePlot)
+  let loadMovieTitle = function () {
+    moviesArray = JSON.parse(localStorage.getItem("movieTitleKey")) || []; //object.keys
 
-            let myMovieYear = document.createElement('p')
-            myMovieYear.textContent = `Year: ${data.Year}`
-            myMovieYear.id = 'div-movie-year'
-            currentMovie.appendChild(myMovieYear)
+    // return movieTitles
+  };
 
-            let myMovieRating = document.createElement('p')
-            myMovieRating.textContent = `Rating: ${data.Rated}`
-            myMovieRating.id = 'div-movie-rating'
-            currentMovie.appendChild(myMovieRating)
-
-            let myMovieGenre = document.createElement('p')
-            myMovieGenre.textContent = `Genre: ${data.Genre}`
-            myMovieGenre.id = 'div-movie-genre'
-            currentMovie.appendChild(myMovieGenre)
-
-            ///created another fetch call to display movie review from NY times API
-
-            fetch(
-                `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${search}&api-key=${nyTimesApiKey}`
-              )
-                .then(function(response) {
-                  return response.json();
-                })
-                .then(function(data) {
-                  console.log(data); //.results[0].link
-       
-                  let movieReview = document.createElement('a');
-                  movieReview.textContent = `Movie Review from NY Times:`;
-                  movieReview.id = 'div-movie-review';
-                  movieReview.setAttribute('href', `${data.results[0].link.url}`);
-                  currentMovie.appendChild(movieReview);
-                });
-
-            
-        })
+  let appendMovieHistory = function () {
+    for (let count = 0; count < moviesArray.length; count++) {
+      let myMovieSearchHistory = document.createElement("button");
+      myMovieSearchHistory.textContent = moviesArray[count];
+      movieSearchHistory.appendChild(myMovieSearchHistory);
     }
+    //let myMovieSearchHistory = document.createElement('li')
+    //   myMovieSearchHistory.textContent = loadMovieTitle()
+    ///  movieSearchHistory.appendChild(myMovieSearchHistory)
+  };
 
-    ////function to save movie titles to local storage, to be updated later
-    let saveMovieTitle = function (search) {
-        localStorage.setItem('movieTitleKey', JSON.stringify(moviesArray));
-    }
+  loadMovieTitle();
+  appendMovieHistory();
 
-    let loadMovieTitle = function () {
-        moviesArray = JSON.parse(localStorage.getItem('movieTitleKey')) || [] //object.keys
+  //let clearMovieHistory = function () {
+  //  localStorage.removeItem('movieTitleKey')
+  // myMovieSearchHistory.textContent = ''
+  //}
 
-       // return movieTitles
+  //let movieSearchHistory = document.querySelector(".movie-history-list")
+  let clearMovieHistory = function () {
+    localStorage.removeItem("movieTitleKey");
+    movieSearchHistory.innerHTML = "";
+  };
 
-    }
-
-   
-
-
-    let appendMovieHistory = function () {
-
-        for (let count = 0; count < moviesArray.length; count++) {
-
-            let myMovieSearchHistory = document.createElement('button')
-            myMovieSearchHistory.textContent = moviesArray[count]
-            movieSearchHistory.appendChild(myMovieSearchHistory)
-
-        }
-        //let myMovieSearchHistory = document.createElement('li')
-     //   myMovieSearchHistory.textContent = loadMovieTitle()
-      ///  movieSearchHistory.appendChild(myMovieSearchHistory)
-
-    }
-
-    loadMovieTitle()
-    appendMovieHistory()
-
-    //let clearMovieHistory = function () {
-      //  localStorage.removeItem('movieTitleKey')
-       // myMovieSearchHistory.textContent = ''
-    //}
-
-
-    //let movieSearchHistory = document.querySelector(".movie-history-list")
-    let clearMovieHistory = function () {
-        localStorage.removeItem('movieTitleKey')
-        movieSearchHistory.innerHTML = ''
-    }
-
-    clearHistory.addEventListener('click', clearMovieHistory)
-   
-})
+  clearHistory.addEventListener("click", clearMovieHistory);
+});
